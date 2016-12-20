983cd8ff81727f2884b12967913ad44d869e6245
Gowri <Gowri.Krishnamoorthy@cimmaintenance.com>
2016-12-20 11:16:08 -0500

    commit from windows

--- Algorithms/src/chapter1/Question1.java
+++ Algorithms/src/chapter1/Question1.java
@@ -24,10 +24,21 @@ class Question1
 		
 	}
 	
+	public void reverse(char[] string)
+	{
+		for(int i=0,j=string.length-1;i<string.length/2;i++,j--)
+		{
+			char current = string[i];
+			string[i] = string[j];
+			string[j] = current;
+		}
+		System.out.println(string + "\n");
+	}
+	
 	public static void main(String[] args)
 	{
 		Question1 q = new Question1();
-		if(q.unique("abca") ==true)
+		if(q.unique("hello") ==true)
 		System.out.println("It contains unique characters");
 		
 		else
