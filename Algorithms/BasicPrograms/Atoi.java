77df2be0edbf45b9e63b83cc06e7a3c266db61fa
Gowri <Gowri.Krishnamoorthy@cimmaintenance.com>
2017-01-11 15:31:30 -0500

    Basic programs

---
+++ Algorithms/BasicPrograms/Atoi.java
@@ -0,0 +1,55 @@
+/*
+ * The numbers 0-9 are consecutive in the Ascii character set. A character is just an 8-bit value.
+ * The characters '0', '1', '2'... have values 48, 49, 50... so subtracting 0 from a digit character gives its integer value.
+ * 
+ * Things to check
+ * 1) Null or empty string
+ * 2) White spaces
+ * 3) Plus or minus sign
+ * 4) Actual conversion to number
+ * 5) Integer overflow limits
+ */
+public class Atoi{	
+	public void convertToInteger(String str){		
+		if(str == null || str.isEmpty()){
+			System.out.println("Invalid string");
+			return;
+		}
+		
+		str = str.trim();
+		
+		int counter = 0;
+		char sign = '+';
+		if(str.charAt(0) == '-'){
+			sign = '-';
+			counter++;
+		}else if(str.charAt(0) == '+'){
+			counter++;
+		}
+		
+		double result  = 0;
+		while(counter < str.length() && str.charAt(counter)>= '0' && str.charAt(counter)<= '9'){
+			int character = str.charAt(counter) - '0';
+			result = result * 10;
+			result = result + character;
+			counter++;
+		}
+		
+		if(sign == '-'){
+			result = -result;
+		}
+		
+		if(result > Integer.MAX_VALUE){
+			result = Integer.MAX_VALUE;
+		}else if(result < Integer.MIN_VALUE){
+			result = Integer.MIN_VALUE;
+		}
+		
+		System.out.println((int)result);
+	}
+	
+	public static void main(String[] args){
+		Atoi atoi = new Atoi();
+		atoi.convertToInteger("   -767");
+	}	
+}
---
+++ Algorithms/BasicPrograms/StringOperations.java
@@ -0,0 +1,86 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class StringOperations {
+
+	public String reverseUsingLoop(String word){
+		char[] charArray = word.toCharArray();
+		for(int i=0, j=charArray.length-1; i<charArray.length/2; i++, j--){
+			char temp = charArray[i];
+			charArray[i] = charArray[j];
+			charArray[j] = temp;			
+		}		
+		return Arrays.toString(charArray);		
+	}
+	
+	public void reverseUsingRecursion(String originalString, String newString){
+		if(originalString.length() != 0){
+			newString = newString.concat(String.valueOf(originalString.charAt(originalString.length()-1)));
+			String newOriginal = originalString.substring(0, originalString.length()-1);
+			reverseUsingRecursion(newOriginal, newString);
+		}else{
+			System.out.println(newString);
+		}
+	}
+	
+	public void isAnagram(String first, String second){
+		HashMap<String, Integer> firstLetters = new HashMap<>();
+		HashMap<String, Integer> secondLetters = new HashMap<>();
+		
+		for(int i=0; i<first.length(); i++){
+			String character = Character.toString(first.charAt(i));
+			if(!firstLetters.containsKey(character)){
+				firstLetters.put(character, 1);
+			}else{
+				Integer oldValue = firstLetters.get(character);
+				firstLetters.put(character, ++oldValue);
+			}
+		}
+		
+		for(int i=0; i<second.length(); i++){
+			String character = Character.toString(second.charAt(i));
+			if(!secondLetters.containsKey(character)){
+				secondLetters.put(character, 1);
+			}else{
+				Integer oldValue = secondLetters.get(character);
+				secondLetters.put(character, ++oldValue);
+			}
+		}
+		
+		if(firstLetters.equals(secondLetters)){
+			System.out.println("Anagram");
+		}else{
+			System.out.println("Not Anagram");
+		}
+	}
+	
+	public void findPatterns(String target){
+		char[] charString = target.toCharArray();
+		if(target.equals("") || target == null){
+			System.out.println("Error. Empty");
+		}else{
+			findPatternsHelper(target);
+		}
+	}
+	
+	private void findPatternsHelper(String target){
+		for(int i=0; i< target.length(); i++){
+			String fixed = Character.toString(target.charAt(i));
+			String newTarget = target.replaceAll(fixed, "");
+			findPatternsHelper2(fixed, newTarget);
+		}
+	}
+	
+	private void findPatternsHelper2(String fixedChar, String target){
+		System.out.print(fixedChar);
+	}
+	
+	
+	public static void main(String[] args){
+		StringOperations stringoperation = new StringOperations();
+		//System.out.println(stringoperation.reverseUsingLoop("hello"));
+		//stringoperation.reverseUsingRecursion("hello", "");
+		
+		stringoperation.isAnagram("top", "pot");
+	}
+}
