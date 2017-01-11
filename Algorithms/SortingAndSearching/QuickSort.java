06ba188333a38cf60c5dd88f666ffae915827a8d
Gowri <Gowri.Krishnamoorthy@cimmaintenance.com>
2017-01-11 11:49:08 -0500

    Comparison between various sorts

--- Algorithms/SortingAndSearching/QuickSort.java
+++ Algorithms/SortingAndSearching/QuickSort.java
@@ -1,4 +1,31 @@
 /*
+ * Uses divide-and-conquer.
+ * 
+ *  The basic step of sorting an array are as follows:
+	1) Select a pivot, normally the middle one
+	2) From both ends, swap elements and make left elements less than pivot and all right greater than pivot.
+	3) Recursively sort left part and right part
+	
+  *  Average time complexity is O(n log(n)).
+  *  Worst case time complexity is O(n^2)
+  *  
+  *  Worst Case: The worst case occurs when the partition process always picks greatest or smallest element as pivot. 
+  *  If we consider above partition strategy where last element is always picked as pivot, the worst case would occur 
+  *  when the array is already sorted in increasing or decreasing order. Following is recurrence for worst case.
+  *  
+  *  The best case occurs when the partition process always picks the middle element as pivot.
+  *  
+  *  Although the worst case time complexity of QuickSort is O(n2) which is more than many other sorting algorithms like Merge Sort and Heap Sort, 
+  *  QuickSort is faster in practice, because its inner loop can be efficiently implemented on most architectures, and in most real-world data.
+  *  QuickSort can be implemented in different ways by changing the choice of pivot, so that the worst case rarely occurs for a given type of data. 
+  *  However, merge sort is generally considered better when data is huge and stored in external storage.
+	
+	Always pick first element as pivot.
+	Always pick last element as pivot (implemented below)
+	Pick a random element as pivot.
+	Pick median as pivot.
+	
+
  * O(log n) - Means repeatedly dividing by a constant. 
  * (Think tree. Every time the number of comparisons gets cut by half)
  * 16 / 2 = 8
