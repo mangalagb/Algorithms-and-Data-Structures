a9f614a7b1a0238e5c322c1c2b85fb98532aa626
Gowri <Gowri.Krishnamoorthy@cimmaintenance.com>
2017-01-18 17:15:11 -0500

    Knapsack problem

--- Algorithms/DynamicProgramming/KnapSack.java
+++ Algorithms/DynamicProgramming/KnapSack.java
@@ -1,21 +1,26 @@
-
 public class KnapSack{
 	
 	//See http://www.geeksforgeeks.org/dynamic-programming-set-10-0-1-knapsack-problem/
-	public int findKnapSack(int capacity, int[] weights, int[] costs, int numberOfItems){
+	public int findKnapSack( int numberOfItems, int capacity, int[] weights, int[] costs){
 		int[][] K = new int[numberOfItems +1][capacity +1];
 		
+		// i = current item.
+		// rollingweight - total weight so far.
+		// weights[i] = weight of the current item. Gets added to the rolling weight in case we decide to keep it.
+		// costs[i] = cost of the current item. Gets added to the knapsack in case we decide to keep it.
+		
 		for(int i=0; i<=numberOfItems; i++){
-			for(int w=0; w<= capacity; w++){
+			for(int rollingWeight=0; rollingWeight<= capacity; rollingWeight++){
 				
-				if(i==0 || w==0){
-					K[i][w] = 0;
+				if(i==0 || rollingWeight==0){
+					K[i][rollingWeight] = 0;
 				}
-				///If current item's weight is less than the cumulative weight,
-				else if(weights[i-1] <= w){
-					K[i][w] = max(costs[i-1] + K[i-1][w - weights[i-1]], K[i-1][w]);
+				///If current item's weight is less than the rollingWeight, we can think about including it
+				else if(weights[i-1] <= rollingWeight){
+					//Max of dont keep it versus keep it and reduce add the weight and cost.
+					K[i][rollingWeight] = max( K[i-1][rollingWeight],  costs[i-1] + K[i-1][rollingWeight - weights[i-1]]);
 				}else{
-					K[i][w] = K[i-1][w];
+					K[i][rollingWeight] = K[i-1][rollingWeight];
 				}
 			}
 		}
@@ -32,6 +37,6 @@ public class KnapSack{
 	    int wt[] = new int[]{10, 20, 30};
 	    int  W = 50;
 	    int n = val.length;
-	    System.out.println(knapsack.findKnapSack(W, wt, val, n));
+	    System.out.println(knapsack.findKnapSack(n ,W, wt, val));
 	}
 }
